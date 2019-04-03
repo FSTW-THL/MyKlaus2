e4d8e8e04eb3943e10446853efae47f6c3eae230
Kai Köster <technik@fstw-thl.de>
2019-04-03 18:34:19 +0200

    Config File hinzugefügt

--- MyKlaus2/MyKlaus2/MyKlaus2/config.py
+++ MyKlaus2/MyKlaus2/MyKlaus2/config.py
@@ -2,3 +2,14 @@ import configparser
 
 
 config = configparser.ConfigParser()
+config.read('settings.ini')
+
+
+class Config:
+    SECRET_KEY = config['DEFAULT']['SECRET_KEY']
+    SQLALCHEMY_DATABASE_URI = config['Database']['URI']
+    MAIL_SERVER = config['Mail']['SERVER']
+    MAIL_PORT = config['Mail']['Port']
+    MAIL_USE_TLS = config['Mail']['SECRET_KEY']
+    MAIL_USERNAME = config['Mail']['USER']
+    MAIL_PASSWORD = config['Mail']['PASS']

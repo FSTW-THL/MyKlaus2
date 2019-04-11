313ee654c500293e0b5f002eb5c65121adc539f0
Denzel-Marlon Kuhlemann <Denzel222@live.de>
2019-04-11 23:29:41 +0200

    Settings Database.URI angepasst | Settings Database.Port hinzugefügt

--- MyKlaus2/MyKlaus2/MyKlaus2/config.py
+++ MyKlaus2/MyKlaus2/MyKlaus2/config.py
@@ -1,16 +1,12 @@
 import configparser
 import os
-from flask import url_for
-
-
 config = configparser.ConfigParser()
-#print({{url_for('static', filename='settings.ini')}})
-#config.read(url_for('static', filename='settings.ini'))
-config.read(os.getcwd()+"\MyKlaus2\static\settings.ini")
+config.read(os.getcwd()+"/MyKlaus2/static/settings.ini")
 
 class Config:
     SECRET_KEY = config['DEFAULT']['SECRET_KEY']
     SQLALCHEMY_DATABASE_URI = config['Database']['URI']
+    SQLALCHEMY_DATABASE_PORT = config['Database']['PORT']
     MAIL_SERVER = config['Mail']['SERVER']
     MAIL_PORT = config['Mail']['Port']
     MAIL_USE_TLS = config['Mail']['SECRET_KEY']

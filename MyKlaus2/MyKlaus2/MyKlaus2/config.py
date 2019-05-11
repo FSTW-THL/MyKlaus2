e7406f536e635de41fef67b1c778098fd3d139f2
Denzel-Marlon Kuhlemann <Denzel222@live.de>
2019-05-11 18:56:39 +0200

    Neue CSS & JS Datein für Tabellen und Notifikationseinblendungen | settings.ini verschoben

--- MyKlaus2/MyKlaus2/MyKlaus2/config.py
+++ MyKlaus2/MyKlaus2/MyKlaus2/config.py
@@ -1,12 +1,15 @@
 import configparser
 import os
 config = configparser.ConfigParser()
-config.read(os.getcwd()+"/MyKlaus2/static/settings.ini")
+config.read(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../settings.ini"))
 
 class Config:
     SECRET_KEY = config['DEFAULT']['SECRET_KEY']
+    DOCUMENT_PATH = config['DEFAULT']['DOCUMENT_PATH']
+
     SQLALCHEMY_DATABASE_URI = config['Database']['URI']
     SQLALCHEMY_DATABASE_PORT = config['Database']['PORT']
+
     MAIL_SERVER = config['Mail']['SERVER']
     MAIL_PORT = config['Mail']['Port']
     MAIL_USE_TLS = config['Mail']['SECRET_KEY']
